814e1980cef29342f51baf6a6cd808c4a450c467
Justin Marsh (:flaviusb) <justin.marsh@flaviusb.net>
2013-01-11 18:41:30 +1300

    Start moving away from using pysec, and towards using my combparse minilib.

--- PreAtomishReader.py
+++ PreAtomishReader.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-from pysec import Parser, choice, quoted_chars, group_chars, option_chars, digits, between, pair, spaces, match, quoted_collection, quoted, many_chars, none_of, ret
+from combparse import *
 
 Universe = {}
 
@@ -17,12 +17,39 @@ def atomish_decimal(num):
 def atomish_whole_number(num):
   return { 'type': 'integer', 'value': int(num) }
 
+def atomish_operator(op):
+  return { 'type': 'operator', value: op }
+
+alpha = char_range("a", "z") + char_range("A", "Z")
+ident = alpha + "_:?!→←⇒⇐$%" + char_range("0", "9")
+
 # AST is a dict
+
+# Mutually recursive functions have to be deffed, they cannot be simple vars
+
+def limb(data, pos, struct):
+  return alt(number, irony, comment, aside)
+
+digit             = relit(r"[0-9]")
+digits            = many1(digit)
+
+decimal_number    = seq(opt(alt(lit("+"), lit("-"))), digits, lit("."), digits)
+whole_number      = seq(opt(alt(lit("+"), lit("-"))), digits)
+number            = alt(decimal_number, whole_number)
+
+# Still uses pysec
 
 expression_choices = []
 expression        = choice(expression_choices)
-decimal_number    = group_chars([choice([option_chars(["-"]), option_chars(["+"])]), digits, match("."), digits]) >> Parser.lift(atomish_decimal)
-whole_number      = group_chars([choice([option_chars(["-"]), option_chars(["+"])]), digits]) >> Parser.lift(atomish_whole_number)
+decimal_number    = join_chars(group([option("", choice([match("-"), match("+")])), digits, match("."), digits])) >> Parser.lift(atomish_decimal)
+whole_number      = join_chars(group([option("", choice([match("-"), match("+")])), digits])) >> Parser.lift(atomish_whole_number)
 number            = choice([decimal_number, whole_number])
-sarcastic_comment = between(match('؟') , many_chars(none_of('\r\n')), ret) >> Parser.lift(atomish_comment('sarcastic'))
-expression_choices.extend([number, sarcastic_comment])
+irony             = between(match('؟') , many_chars(none_of('\r\n')), ret) >> Parser.lift(atomish_comment('ironic'))
+comment           = between(match('##') , many_chars(none_of('\r\n')), ret) >> Parser.lift(atomish_comment('earnest'))
+aside             = between(match('#-') , many_chars(none_of('-#')), match('-#')) >> Parser.lift(atomish_comment('aside'))
+
+name              = join_chars(group([one_of(alpha), many_chars(ident, min_count = 0)]))
+operator          = many_chars('~!@$%^&*_=\'`/?×÷+-', min_count = 1) >> Parser.lift(atomish_operator)
+
+interpolated      = group([match('#{')
+expression_choices.extend([number, irony, comment, aside])
