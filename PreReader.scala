5f1184af905b0e524c011b2200b07456d96a0338
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2013-04-13 01:15:39 +1200

    Change parser ordering around to avoid ints and decimals being swallowed.

--- PreReader.scala
+++ PreReader.scala
@@ -19,7 +19,7 @@ object AtomishParser extends RegexParsers {
   def rational = "[+-]?[0-9]+\\.[0-9]+".r ^^ { (double: String) => AtomishDecimal(double.toDouble) }
   def integer  = "[+-]?[0-9]+".r ^^ { (int: String) => AtomishInt(int.toInt) }
   def identifier: Parser[AtomishMessage] = ("[a-zA-Z][a-zA-Z0-9_:$!?]*".r | "[~!@$%^&*_=\'`/?×÷+-]*".r) ^^ { AtomishMessage(_) }
-  def code: Parser[AtomishCode] = commated | atomish_call | identifier | rational | integer // This will eventually be a big union of all types that can constitute standalone code
+  def code: Parser[AtomishCode] = (commated | atomish_call | rational | integer | identifier) // This will eventually be a big union of all types that can constitute standalone code
   def commated_bit: Parser[List[AtomishCode]] = (("," ~ code)*) ^^ { _.map { case "," ~ frag => frag } }
   def commated: Parser[AtomishCommated] = "(" ~ ((code ~ commated_bit)?) ~ ")" ^^ {
     case opb ~ Some((code1: AtomishCode) ~ (code2: List[AtomishCode])) ~ opc => AtomishCommated(Array[AtomishCode](code1) ++
