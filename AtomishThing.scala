dd83e330197901b04a91a4690db80c9723db69af
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2013-04-12 22:47:49 +1200

    Start adding AlienProxies.

--- AtomishThing.scala
+++ AtomishThing.scala
@@ -5,12 +5,34 @@ import scala.collection.mutable.{Map => MMap}
 // AtomishThing is the supertype of all the container types for all 'things' that are reified in Atomish
 trait AtomishThing {
   var cells: MMap[String, AtomishThing] = MMap(
+    "hasCell" -> AlienProxy[AtomishString, AtomishBoolean](x => AtomishBoolean(cells.isDefinedAt(x.value))),
+    "cell" -> AlienProxy[AtomishString, AtomishThing](x => cells(x.value)),
+    "activatable" -> AtomishBoolean(false)
   )
 }
 
-case class AtomishInt(value: Int) extends AtomishThing with AtomishCode
+case class AtomishBoolean(value: Boolean) extends AtomishThing with AtomishCode {
+}
 
-case class AtomishDecimal(value: Double) extends AtomishThing with AtomishCode
+case class AtomishInt(value: Int) extends AtomishThing with AtomishCode {
+  cells ++= MMap[String, AtomishThing](
+    "+" -> AlienProxy[AtomishInt, AtomishInt](x => AtomishInt(x.value + value)),
+    "-" -> AlienProxy[AtomishInt, AtomishInt](x => AtomishInt(x.value - value)),
+    "×" -> AlienProxy[AtomishInt, AtomishInt](x => AtomishInt(x.value * value)),
+    "÷" -> AlienProxy[AtomishInt, AtomishInt](x => AtomishInt(x.value / value))
+  )
+}
+
+case class AtomishDecimal(value: Double) extends AtomishThing with AtomishCode {
+  cells ++= MMap[String, AtomishThing](
+    "+" -> AlienProxy[AtomishDecimal, AtomishDecimal](x => AtomishDecimal(x.value + value)),
+    "-" -> AlienProxy[AtomishDecimal, AtomishDecimal](x => AtomishDecimal(x.value - value)),
+    "×" -> AlienProxy[AtomishDecimal, AtomishDecimal](x => AtomishDecimal(x.value * value)),
+    "÷" -> AlienProxy[AtomishDecimal, AtomishDecimal](x => AtomishDecimal(x.value / value))
+ )
+}
+
+case class AtomishString(value: String) extends AtomishThing with AtomishCode
 
 case class AtomishMessage(name: String) extends AtomishThing with AtomishCode
 
@@ -21,3 +43,5 @@ case class AtomishCall(name: String, args: Array[AtomishCode]) extends AtomishTh
 case class MessageChain(messages: Array[AtomishMessage]) extends AtomishThing with AtomishCode
 
 trait AtomishCode
+
+case class AlienProxy[A, B](call: A => B) extends AtomishThing
