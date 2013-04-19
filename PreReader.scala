b55d1d1a41d208bad811a2fe230fb84bd6f6bddc
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2013-04-20 00:13:11 +1200

    Make parser handle whitespace in commateds.

--- PreReader.scala
+++ PreReader.scala
@@ -25,7 +25,6 @@ object AtomishParser extends RegexParsers {
   def string   = ("\"" ~ """([^"\\]|\\")*""".r ~ "\"") ^^ { case "\"" ~ str ~ "\"" => AtomishString(str) }
   def identifier: Parser[AtomishMessage] = ("[a-zA-Z][a-zA-Z0-9_:$!?]*".r | "[~!@$%^&*_=\'`/?×÷+-]+".r) ^^ { AtomishMessage(_) }
   def code_tiny_bit: Parser[AtomishCode] = (commated | atomish_call | string | rational | integer | identifier | nll) // This will eventually be a big union of all types that can constitute standalone code
-  //def code_tiny_bit: Parser[AtomishCode] = (nll) // This will eventually be a big union of all types that can constitute standalone code
   def code_bit: Parser[List[AtomishCode]] = (((nll ~ code_tiny_bit) | nll | (wss ~ code_tiny_bit))*) ^^ { _.flatMap { 
     case "" ~ (code_piece: AtomishCode)        => List(code_piece)
     case AtomishNL ~ (code_piece: AtomishCode) => List(AtomishNL, code_piece)
@@ -34,11 +33,11 @@ object AtomishParser extends RegexParsers {
   def code: Parser[AtomishForm] = code_tiny_bit ~ code_bit ^^ {
     case a ~ b => AtomishForm(a::b)
   }
-  def commated_bit: Parser[List[AtomishCode]] = (("," ~ code)*) ^^ { _.map { case "," ~ frag => frag } }
-  def commated: Parser[AtomishCommated] = "(" ~ ((code ~ commated_bit)?) ~ ")" ^^ {
-    case opb ~ Some((code1: AtomishCode) ~ (code2: List[AtomishCode])) ~ opc => AtomishCommated(Array[AtomishCode](code1) ++
+  def commated_bit: Parser[List[AtomishCode]] = (("," ~ (wss?) ~ code)*) ^^ { _.map { case "," ~ x ~ frag => frag } }
+  def commated: Parser[AtomishCommated] = "(" ~ (wss?) ~ ((code ~ (wss?) ~ commated_bit)?) ~ (wss?) ~ ")" ^^ {
+    case opb ~ wsa ~ Some((code1: AtomishCode) ~ wsb ~ (code2: List[AtomishCode])) ~ wsc ~ opc => AtomishCommated(Array[AtomishCode](code1) ++
       (code2.toArray[AtomishCode]))
-    case opb ~ None ~ opc => AtomishCommated(Array())
+    case opb ~ wsa ~ None ~ wsb ~ opc => AtomishCommated(Array())
   }
   def atomish_call: Parser[AtomishCall] = identifier ~ commated ^^ {
     case AtomishMessage(name) ~ AtomishCommated(args) => AtomishCall(name, args)
