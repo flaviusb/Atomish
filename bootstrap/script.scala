c7b6b6745956ef39598475d28e421ab5f8e56d74
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2013-05-18 02:29:56 +1200

    Add in a script for poking at pre-Atomish from the Scala repl.

---
+++ bootstrap/script.scala
@@ -0,0 +1,21 @@
+import net.flaviusb.atomish._
+
+var u = new PreUniverse()
+var r = new PreReader()
+var e = PreEvaller.eval(u) _
+
+u.roots("read") = r.alien_read;
+u.roots("eval") = AlienProxy(_.args match {
+  case List(Left(arg: AtomishCode))                           => e(arg, None)
+  case List(Left(arg: AtomishCode), Left(base: AtomishThing)) => e(arg, Some(base))
+  case _                                                      => null // Should error
+})
+u.roots("print") = AlienProxy(_.args match {
+  case List(Left(arg: AtomishThing)) => AtomishString(PreScalaPrinter.print(arg))
+  case _                             => null // Should error
+})
+u.roots("print_with_forms") = AlienProxy(_.args match {
+  case List(Left(arg: AtomishThing)) => AtomishString(PreScalaPrinter.print_with_forms(arg))
+  case _                             => null // Should error
+})
+
