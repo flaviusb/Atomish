d596908a5d80cec325fdf95d5168588fe27a5440
Justin (:flaviusb) Marsh <justin.marsh@flaviusb.net>
2013-06-07 21:46:29 +1200

    Add idempotent? fn on PreScalaMirror.

--- bootstrap/PreMirror.scala
+++ bootstrap/PreMirror.scala
@@ -3,8 +3,12 @@ package net.flaviusb.atomish
 import scala.collection.mutable.{Map => MMap}
 
 object PreScalaMirror {
+  // This mirror only works on quote protected arguments.
   val mirror = Origin(with(MMap[String, AtomishThing](
-    "idempotent?" -> QAlienProxy(ctd => { AtomishUnset }),
+    "idempotent?" -> QAlienProxy(_.args match {
+      case AtomishCall("'", Array(x: IdempotentEval)) => AtomishBoolean(true)
+      case _                                          => AtomishBoolean(false)
+    }),
     "cells"       -> QAlienProxy(ctd => { AtomishUnset }),
     "code?"       -> QAlienProxy(ctd => { AtomishUnset }),
     "origin?"     -> QAlienProxy(ctd => { AtomishUnset }),
